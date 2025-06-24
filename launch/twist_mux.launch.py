19d284556bf21eb9d13989ee376e240ef4e778f1
pboon09 <pboonchanachai@gmail.com>
2025-06-24 12:22:58 +0700

    Update twist_mux.launch.py

--- launch/twist_mux.launch.py
+++ launch/twist_mux.launch.py
@@ -1,12 +1,21 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    pkg_share = get_package_share_directory('twist_mux')
+    config_path = os.path.join(pkg_share, 'config', 'config.yaml')
+    
+    twist_mux_node = Node(
+        package='twist_mux',
+        executable='twist_mux.py',
+        name='twist_mux',
+        parameters=[config_path],
+        output='screen',
+        emulate_tty=True,
+    )
+    
     return LaunchDescription([
-        Node(
-            package='twist_mux',
-            executable='twist_mux.py',
-            name='twist_mux',
-            parameters=['/home/pboon09/ros2_ws/src/twist_mux/config/config.yaml']
-        )
+        twist_mux_node
     ])
