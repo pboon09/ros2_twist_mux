1a54a9747a4049b142e558b133827f54547a58b8
pboon09 <pboonchanachai@gmail.com>
2025-07-15 11:59:52 +0700

    fix name 2

--- scripts/dummy_script.py
+++ scripts/dummy_script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from twist_mux.dummy_module import dummy_function, dummy_var
+from ros2_twist_mux.dummy_module import dummy_function, dummy_var
 import rclpy
 from rclpy.node import Node
 
